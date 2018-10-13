0ea7d0d7c996ab539fbbc652027519305fe7d021
Ihor V <iveselovskyy@outlook.com>
2018-10-13 19:28:37 -0400

    Removed unused file

--- backend/ipfsUpload.js
+++
@@ -1,9 +0,0 @@
-
-var ipfsAPI = require('ipfs-api')
-var ipfs = ipfsAPI('/ip4/127.0.0.1/tcp/5001')
-
-ipfs.util.addFromURL('http://ipfs.io/', (err, result) => {
-    if (err) { throw err }
-    console.log(result)
-})
-

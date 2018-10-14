f165388f51dd033d3cfbf172df2cbef43ffc183e
Ihor V <iveselovskyy@outlook.com>
2018-10-13 21:50:48 -0400

    Added contract module

--- exporter/deploy.js
+++ exporter/deploy.js
@@ -1,4 +1,4 @@
-const HDWalletProvider = require('truffle-hd-wallet-provider');
+const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
 const { interface, bytecode } = require('./compile');
 const secrets = require('./secrets');
--- exporter/secrets.js
+++ exporter/secrets.js
@@ -1,4 +1,5 @@
 module.exports = {
-    wallet: 'test',
-    infuraApi: 'api',
+    wallet: 'turn cancel one loud gate across legal obscure orchard extra salad hire',
+    infuraApi: 'https://rinkeby.infura.io/v3/c857e50ebbc84418a2679110c1357e66',
+    address: '0x55a860e4fb2D016D3b07765a2dAD34DD70B7aDa4',
 }
